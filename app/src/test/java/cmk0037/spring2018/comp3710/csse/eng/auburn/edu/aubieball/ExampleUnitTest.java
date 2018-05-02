1a21dee4faec3810dcff03e78167bbd3ac1a3e10
Cameron Kubik <camkubikau@gmail.com>
2018-05-02 16:48:15 -0500

    initial commit

---
+++ app/src/test/java/cmk0037/spring2018/comp3710/csse/eng/auburn/edu/aubieball/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package cmk0037.spring2018.comp3710.csse.eng.auburn.edu.aubieball;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
