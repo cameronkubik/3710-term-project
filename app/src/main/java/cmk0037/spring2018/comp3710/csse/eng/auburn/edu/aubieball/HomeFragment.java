00e3423833ff25e74772b3c0999dd5c5f408e6d8
Cameron Kubik <camkubikau@gmail.com>
2018-05-03 04:00:02 -0500

    wired button clicks to home fragment, moving on to building game fragment

--- app/src/main/java/cmk0037/spring2018/comp3710/csse/eng/auburn/edu/aubieball/HomeFragment.java
+++ app/src/main/java/cmk0037/spring2018/comp3710/csse/eng/auburn/edu/aubieball/HomeFragment.java
@@ -6,13 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
- * Created by camkubikpro on 5/3/18.
+ * Created by Cameron Kubik on 5/3/18.
  */
 
 public class HomeFragment extends Fragment {
-
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,6 +23,22 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
+        Button mStartButton = v.findViewById(R.id.start_game_button);
+        Button mScoreButton = v.findViewById(R.id.view_score_button);
+
+        // On click - Start Game Button
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Replace home content fragment with game fragment
+            }
+        });
+        // On click - View Score Records Button
+        mScoreButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Replace home fragment with score fragment
+            }
+        });
+
         return v;
     }
 }
